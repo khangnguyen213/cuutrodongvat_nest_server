4b4ba66d63df71bd62ec8e2089b4bd26100bd050
KhangNguyen <khangnguyenm01@gmail.com>
2024-04-02 10:49:07 +0700

    add vercel.json

--- src/main.ts
+++ src/main.ts
@@ -14,8 +14,6 @@ async function bootstrap() {
   );
   app.enableCors({
     origin: 'http://localhost:5173/',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
   });
   app.useStaticAssets('public');
 
