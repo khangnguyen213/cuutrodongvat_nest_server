ef442b82640dd0c275ff96d89f0597909779e45b
KhangNguyen <khangnguyenm01@gmail.com>
2024-04-02 11:47:43 +0700

    change db

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,8 @@ async function bootstrap() {
     }),
   );
   app.enableCors({
-    origin: 'http://localhost:5173/',
+    //allow everywhere
+    origin: '*',
   });
   app.useStaticAssets('public');
 
